7d64ed19ddbdfa29e75af9e20ed0e7a71654831d
malec <alec@ahlbrandt.net>
2019-02-22 17:54:01 -0600

    private key generated

--- keyGenRSA.py
+++ keyGenRSA.py
@@ -7,5 +7,17 @@ private_key = rsa.generate_private_key(
     public_exponent=65537, key_size=2048, backend=default_backend()
 )
 
-with open(sys.argv[1], 'wb') as file:
-    file.write(private_key.public_key().public_bytes('utf-8', serialization.PublicFormat.SubjectPublicKeyInfo))
+try:
+    pubFileName=sys.argv[2]
+except IndexError:
+    pubFileName = 'id_rsa.pub'
+
+try:
+    privateFileName=sys.argv[1]
+except IndexError:
+    privateFileName = 'id_rsa'
+
+with open(pubFileName, 'wb') as file:
+    file.write(private_key.public_key().public_bytes(serialization.Encoding.PEM, serialization.PublicFormat.SubjectPublicKeyInfo))
+with open(privateFileName, 'wb') as file:
+    file.write(private_key.private_bytes(serialization.Encoding.PEM, serialization.PrivateFormat.TraditionalOpenSSL, serialization.NoEncryption()))
