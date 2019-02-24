3304348773d3dc04f0c2bc6f9d6e66dfd3b2172c
Alec Ahlbrandt <alec@ahlbrandt.net>
2019-02-23 19:12:25 -0600

    give error message

--- keyGenAES.py
+++ keyGenAES.py
@@ -3,6 +3,10 @@ import sys
 def generateKey():
     return Fernet.generate_key()
 
-outputFlie = sys.argv[1]
+try:
+    outputFlie = sys.argv[1]
+except IndexError:
+    print "Provide an output file as a command line argument."
+    exit()
 with open(outputFlie, "wb") as file:
     file.write(generateKey())
