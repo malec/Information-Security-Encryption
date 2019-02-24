dec0d84a10eb4fa09bde153d5540b0ca16b2d2b5
Alec Ahlbrandt <alec@ahlbrandt.net>
2019-02-23 20:05:49 -0600

    test time added

--- part3/index.py
+++ part3/index.py
@@ -2,6 +2,7 @@ import sys, time, datetime
 File_Name_AES_Key = "key"
 File_Name_RSA_Key = "id_rsa"
 File_Name_RSA_Pub_Key = "id_rsa.pub"
+count=1
 try:
     input = sys.argv[1]
     if(len(input) > 7):
@@ -13,23 +14,27 @@ except IndexError:
 
 # Start stopwatch to measure AES performance
 AES_start_time = time.time()
-# AES Encrypt and Decrypt 100 times
-for(x in range(0,100)):
+# AES Encrypt and Decrypt count times
+for x in range(0, count):
     # do encryption and decryption
+    time.sleep(3)
 
+# calculate average time
 AES_end_time = time.time()
 AES_uptime = AES_end_time - AES_start_time
-AES_human_uptime = str(datetime.timedelta(seconds=int(AES_uptime)))
+AES_average_time = str(datetime.timedelta(seconds=int(AES_uptime/count)))
 
 # Start stopwatch to measure RSA performance
 RSA_start_time = time.time()
-# RSA Encrypt and Decrypt 100 times
-for(x in range(0,100)):
+# RSA Encrypt and Decrypt count times
+for x in range(0, count):
     # do encryption and decryption
-    
+    time.sleep(3)
+
 RSA_end_time = time.time()
 RSA_uptime = RSA_end_time - RSA_start_time
-RSA_human_uptime = str(datetime.timedelta(seconds=int(RSA_uptime)))
+RSA_average_time = str(datetime.timedelta(seconds=int(RSA_uptime/count)))
 
 # Print Averages
-print("Average AES encryption and decryption time: " + AES_human_uptime/100)
+print("Average AES encryption and decryption time: " + AES_average_time)
+print("Average RSA encryption and decryption time: " + RSA_average_time)
